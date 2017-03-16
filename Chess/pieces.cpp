f1c9dcb54f1c400a6a53f23dadd95bb268844f86
Sean Jones <sjones7@stevens.edu>
2017-03-16 17:21:16 -0400

    updated chess game mechanics

--- Chess/pieces.cpp
+++ Chess/pieces.cpp
@@ -1,26 +1,96 @@
 #include "pieces.h"
 
-bool king::allowedMove(int srow, int scol, int erow, int ecol){
-    
+bool Piece::checkPawn(int endRow, int endCol, bool occupied){
+    switch(color){
+        case 0:{ //white pieces increasing numbers in rows
+            if (occupied == false){ // can't move forward into opposing piece
+                if (endRow == row + 1 && endCol == col)
+                    return true;
+                if (row == 1){ // can move forward two spaces off start
+                    if (endRow == row + 2 && endCol == col)
+                        return true;
+                }
+            }
+            else { // can only capture diagonally
+                if (endRow == row + 1 && endCol == col - 1)
+                    return true;
+                if (endRow == row + 1 && endCol == col + 1)
+                    return true;
+            }
+            break;
+        }
+        case 1:{ //black pieces decreasing numbers in rows
+            if (occupied == false){
+                if (endRow == row - 1 && endCol == col)
+                    return true;
+                if (row == 6){
+                    if (endRow == row - 2 && endCol == col)
+                        return true;
+                }
+            }
+            else {
+                if (endRow == row - 1 && endCol == col - 1)
+                    return true;
+                if (endRow == row - 1 && endCol == col + 1)
+                    return true;
+            }
+            break;
+        }
+    }
+    return false;
 }
 
-bool piece::checkValid(piece p, int endRow, int endCol, int endPieceColor){
-    if(p.color == endPieceColor){
+bool Piece::checkRook(int endRow, int endCol){
+    return false;
+}
+
+bool Piece::checkKnight(int endRow, int endCol){
+    return false;
+}
+
+bool Piece::checkKing(int endRow, int endCol){
+    if (endRow == row){
+        if (endCol == col - 1 || endCol == col + 1)
+            return true; // checks move 1 to left or right
+    }
+    if (endCol == col){
+        if (endRow == row - 1 || endRow == row + 1)
+            return true; // checks move 1 up or back
+    }
+    if ((endRow == row + 1 && endCol == col - 1) || (endRow == row - 1 && endCol == col + 1)){
+        return true; // checks top left corner and bottom right corner
+    }
+    if ((endRow == row - 1 && endCol == col + 1) || (endRow == row + 1 && endCol == col - 1)){
+        return true; // checks top right corner and bottom left corner
+    }
+    return false;
+}
+
+bool Piece::checkQueen(int endRow, int endCol){
+    return true;
+}
+
+bool Piece::checkBishop(int endRow, int endCol){
+    return false;
+}
+
+bool Piece::checkValid(int endRow, int endCol, int endPieceColor, bool occupied){
+    if(color == endPieceColor){ // can not take own color piece
         return false;
     }
     bool allow = false;
-    swtich(p.name){
-        case 'P': allow = pawn::allowedMove(p.row, p.col, endRow, endCol);
+    switch(name){
+        case 'P': allow = checkPawn(endRow, endCol, occupied);
             break;
-        case 'R': allow = rook::allowedMove(p.row, p.col, endRow, endCol);
+        case 'R': allow = checkRook(endRow, endCol);
             break;
-        case 'H': allow = knight::allowedMove(p.row, p.col, endRow, endCol);
+        case 'H': allow = checkKnight(endRow, endCol);
             break;
-        case 'K': allow = king::allowedMove(p.row, p.col, endRow, endCol);
+        case 'K': allow = checkKing(endRow, endCol);
             break;
-        case 'Q': allow = queen::allowedMove(p.row, p.col, endRow, endCol);
+        case 'Q': allow = checkQueen(endRow, endCol);
             break;
-        case 'B': allow = bishop::allowedMove(p.row, p.col, endRow, endCol);
+        case 'B': allow = checkBishop(endRow, endCol);
             break;
    }
    return allow;
--- Chess/pieces.h
+++ Chess/pieces.h
@@ -1,49 +1,22 @@
 #ifndef PIECES_H
 #define PIECES_H
 
-class piece: public Chess{
+#include "chess.h"
+
+class Piece: public Chess{
 private:
-    int color, row, col;
+    int color;
     char name;
+    int row, col;
 public:
-    piece(int color, char name, int row, int col): color(color), name(name), row(row), col(col){}
-    bool checkValid(piece p, int endRow, int endCol, int endPieceColor);
-};
-
-class king: public piece{
-public:
-    king(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
-};
-
-class queen: public piece{
-public:
-    queen(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
-};
-
-class rook: public piece{
-public:
-    rook(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
-};
-
-class bishop: public piece{
-public:
-    bishop(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
-};
-
-class knight: public piece{
-public:
-    knight(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
-};
-
-class pawn: public piece{
-public:
-    pawn(){}
-    bool allowedMove(int srow, int scol, int erow, int ecol);
+    Piece(int color, char name, int row, int col): color(color), name(name), row(row), col(col){}
+    bool checkValid(int endRow, int endCol, int endPieceColor, bool occupied);
+    bool checkPawn(int endRow, int endCol, bool occupied);
+    bool checkRook(int endRow, int endCol);
+    bool checkKnight(int endRow, int endCol);
+    bool checkKing(int endRow, int endCol);
+    bool checkQueen(int endRow, int endCol);
+    bool checkBishop(int endRow, int endCol);
 };
 
 #endif
