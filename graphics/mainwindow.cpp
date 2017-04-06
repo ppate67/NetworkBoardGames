11fbc567645ddc498a982985ea089f16aa58e5b0
lx2054807 <lx199317@foxmail.com>
2017-04-06 16:27:50 -0400

    Add files via upload

--- graphics/mainwindow.cpp
+++ graphics/mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -18,7 +19,7 @@ click playing checker
 */
 void MainWindow::on_pushButton_2_clicked()
 {
-
+    connect(this, SIGNAL (clicked()), this, SLOT (checker::slot1()));
 }
 
 /*
@@ -26,7 +27,7 @@ click playing chess
 */
 void MainWindow::on_pushButton_3_clicked()
 {
-
+    connect(this, SIGNAL (clicked()), this, SLOT (chess::slot1()));
 }
 
 /*
@@ -34,7 +35,7 @@ click playing go
 */
 void MainWindow::on_pushButton_4_clicked()
 {
-
+    connect(this, SIGNAL (clicked()), this, SLOT (go::slot1()));
 }
 
 /*
@@ -42,7 +43,7 @@ click playing monopoly
 */
 void MainWindow::on_pushButton_5_clicked()
 {
-
+    connect(this, SIGNAL (clicked()), this, SLOT (monopoly::slot1()));
 }
 
 /*
@@ -50,5 +51,5 @@ click quiting game
 */
 void MainWindow::on_pushButton_6_clicked()
 {
-
+    connect(this, SIGNAL (clicked()), this, SLOT (quit()));
 }
