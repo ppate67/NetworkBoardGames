0915354ba814bbd8e9d9dc21a2b9b8596cfa00f7
nnuila115 <nnuila@stevens.edu>
2017-04-07 14:17:41 -0400

    Added chatserver functions to the myserver.h files below the classes. see comments in code.

--- Networking/Server/myserver.h
+++ Networking/Server/myserver.h
@@ -10,6 +10,13 @@
 #include <player.h>
 #include <QMap>
 #include <QSet>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
 //#include <iostream>
 
 //using namespace std;
@@ -28,9 +35,6 @@ public:
     int getNumCheckGames();
     int getCheckGamePeer(int game);
     int getNumMonoGames();
-    void readyRead();
-    void disconnected();
-    void sendUserList();
 
     vector<int> getMonoGamePeer(int game);
 protected:
@@ -42,8 +46,68 @@ public slots:
 
 private:
     int numGo,numChess,numCheck,numMono;
-    QSet<QTcpSocket*> clients;
-    QMap<QTcpSocket*,QString> users;
 };
+
+void newconnection (int sock)
+{
+   int n;
+   char buffer[256];
+
+   bzero(buffer,256);
+   n = read(sock,buffer,255);
+   if (n < 0) error("Error reading from socket");
+   printf("playerName: %s\n",buffer); // playername in quotes here will be replaeced wiht actual playername
+   //n = write(sock,"Message sent",18); this needs to go to the UI eventually
+
+   //this is where we pass n to the message board and append the username to the beginning
+   //of the string. ie, username:message
+}
+
+void error(const char *errormsg)
+{     //standardize error messages
+    perror(errormsg);
+    exit(1);
+}
+
+void StartChatServer(){
+  int sockfd, newsockfd, pid;
+  int portnum = 51717;
+  socklen_t clientlength;
+  struct sockaddr_in serv_addr, cli_addr;
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd < 0)
+     error("Error opening socket");
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portnum);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr,
+           sizeof(serv_addr)) < 0)
+           error("Error on binding");
+  listen(sockfd,5);
+
+  clientlength = sizeof(cli_addr);
+
+  while (1) {
+      newsockfd = accept(sockfd,
+            (struct sockaddr *) &cli_addr, &clientlength);
+      if (newsockfd < 0)
+          error("Error on accept");
+      pid = fork();
+      if (pid < 0)
+          error("Error on fork");
+      if (pid == 0)  {
+          close(sockfd);
+          newconnection(newsockfd);
+          exit(0);
+      }
+      else close(newsockfd);
+  }
+  close(sockfd);
+  return 0;
+}
 
 #endif // MYSERVER_H
