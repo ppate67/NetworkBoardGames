2f6a7f338b6d6ad8e2776b1971e523147f763d7b
Joe Puciloski <jpucilos@stevens.edu>
2017-04-09 21:38:57 -0400

    changed board png to jpg, added boardSpaces and player header files, and initialized spaces in main.ccp

---
+++ Monopoly/monopoly_graphics/boardspace.h
@@ -0,0 +1,79 @@
+#ifndef BOARDSPACE_H
+#define BOARDSPACE_H
+
+#include<string>
+using namespace std;
+
+class boardSpace{
+private:
+    string name;
+    string actionText;
+    string spaceType;
+    int price;
+    int freeParking;
+    bool occupied;
+    int owned;
+    bool mortgaged;
+    int houses;
+    int rent;
+    int tax;
+public:
+    boardSpace(){} // everything default to zero
+
+    //use void functions such as these to allow us to create an array of board positions easier
+    //we will set us this array in main.cpp as part of initialization
+    void setName(string initName){
+        name = initName;
+    }
+    void setType(string initType){
+        spaceType = initType;
+    }
+    void setPropertyCost(int initPrice){
+        price = initPrice;
+    }
+
+    void setFreeParking(int initMoney){
+        freeParking = initMoney;
+    }
+
+    void setOccupied(bool initOccupied){
+        occupied = initOccupied;
+    }
+
+    void setOwnership(int initOwnership){
+        owned = initOwnership;
+    }
+
+    void setMortgaged(bool initMortgage){
+        mortgaged = initMortgage;
+    }
+
+    void setRent(int initRent){
+        rent = initRent;
+    }
+    void setTax(int initTax){
+        tax = initTax;
+    }
+
+    void setActionText(string initText){
+        actionText = initText;
+    }
+
+    void setHouses(int initHouses){
+        houses = initHouses;
+    }
+
+    bool upgrade(){
+        if(houses < 5){
+            rent = rent * 2;
+            houses++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+};
+
+
+#endif // BOARDSPACE_H
