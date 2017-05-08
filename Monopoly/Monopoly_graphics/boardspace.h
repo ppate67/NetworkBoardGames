99470f38d5a5b71f4cf841091740ad1126b9059c
Taylor <taylor.sinatra@dronegohome.tech>
2017-05-08 17:23:56 -0400

    Moving files from different directory to support Linux case sensitivy

---
+++ Monopoly/Monopoly_graphics/boardspace.h
@@ -0,0 +1,138 @@
+#ifndef BOARDSPACE_H
+#define BOARDSPACE_H
+#include"player.h"
+#include<string>
+using namespace std;
+
+class boardSpace{
+private:
+    string name;
+    string actionText;
+    string spaceType;
+    int Ownership;
+    int price;
+    int freeParking;
+    //bool occupied;
+    int owned;
+    bool mortgaged;
+    int houses;
+    int rent;
+    int tax;
+    int xCoordinate;
+    int yCoordinate;
+public:
+    boardSpace(){} // everything default to zero, set parameters outside of class with method calls
+
+    int getRent(){
+        return rent;
+    }
+    string getSpaceType(){
+        return spaceType;
+    }
+    int getOwnership(){
+        return Ownership;
+    }
+
+    int getPrice(){
+        return price;
+    }
+
+    friend void setOwner(boardSpace& a, int id){
+        a.owned = id;
+    }
+
+    friend void buildHouse(boardSpace& a){
+        a.houses = a.houses +1;
+        a.rent = a.rent + a.houses*100 ;
+    }
+
+    friend void mortgageProperty(boardSpace& a){
+        a.mortgaged = true;
+    }
+
+    friend void unmortgageProperty(boardSpace& a){
+        a.mortgaged = false;
+    }
+
+    //use void functions such as these to allow us to create an array of board positions easier
+    void setName(string initName){
+        name = initName;
+    }
+    void setType(string initType){
+        spaceType = initType;
+    }
+    void setPropertyCost(int initPrice){
+        price = initPrice;
+    }
+
+    void setFreeParking(int initMoney){
+        freeParking = initMoney;
+    }
+
+    /*
+    void setOccupied(bool initOccupied){
+        occupied = initOccupied;
+    }
+    */
+
+    void setOwnership(int initOwnership){
+        Ownership = initOwnership;
+    }
+
+    void setMortgaged(bool initMortgage){
+        mortgaged = initMortgage;
+    }
+
+    void setRent(int initRent){
+        rent = initRent;
+    }
+    void setTax(int initTax){
+        tax = initTax;
+    }
+
+    void setActionText(string initText){
+        actionText = initText;
+    }
+
+    void setHouses(int initHouses){
+        houses = initHouses;
+    }
+
+    void setXcoordinate(int initX){
+        xCoordinate = initX;
+    }
+
+    void setYcoordinate(int initY){
+        yCoordinate = initY;
+    }
+
+    bool upgrade(){
+        if(houses < 5){
+            rent = rent * 2;
+            houses++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    int getX(){
+        return xCoordinate;
+    }
+
+    int getY(){
+        return yCoordinate;
+    }
+
+    string getBoardname(const boardSpace& a){
+        return a.name;
+    }
+
+    string getName(){
+        return name;
+    }
+
+};
+
+
+#endif // BOARDSPACE_H
---
+++ Monopoly/Monopoly_graphics/monopoly.pro
@@ -0,0 +1,23 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = monopoly
+TEMPLATE = app
+
+DEFINES += QT_DEPRECATED_WARNINGS
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h \
+    player.h \
+    boardspace.h \
+    cards.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    resources.qrc
